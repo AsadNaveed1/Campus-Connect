b3ecaf95f64ca830c213125f3abeee78c4a654e2
Arctic Kangaroo <theanubhavkashyap@gmail.com>
2024-11-27 02:32:18 +0800

    Add skeleton for QR scanner

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -19,6 +19,7 @@ export default function RootLayout() {
               <Stack.Screen name="index" options={{ headerShown: false }} />
               <Stack.Screen name="register" options={{ headerShown: false }} />
               <Stack.Screen name="search" options={{ headerShown: false }} />
+              <Stack.Screen name="qrScanner" options={{ headerShown: false }} />
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="eventPage" options={{ headerShown: false }} />
               <Stack.Screen name="societyPage" options={{ headerShown: false }} />
--- app/search.jsx
+++ app/search.jsx
@@ -65,6 +65,7 @@ export default function Search() {
   };
 
   const handleQRButton = () => {
+    router.replace("qrScanner");
   };
 
   return (
